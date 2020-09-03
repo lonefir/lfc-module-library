2e0d656ac447f08a060ea11fd866eb9d264414fc
Stephen Karpeles <stephen@stephenkarpeles.com>
2020-09-03 11:05:26 -0500

    FAQs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Accordion
+const accordionContainers = document.querySelectorAll('.accordion-container');
+
+for (let i = 0; i < accordionContainers.length; i++) {
+  let accordionContainer = accordionContainers[i];
+  accordionContainer.addEventListener('click', function() {
+    this.classList.toggle('is-open-accordion');
+  });
+}
+
 // Logo Bar Slider
 $(document).ready(function(){
   $('.logo-bar__slider').slick({
